be6180d0aaf5df1f8fc077ba4e6fe23b3563ffdc
Luka <jovanovic.luka1717@gmail.com>
2022-06-14 12:39:33 +0200

    Trained a convolutional model, and fixed an image dimension bug

--- solution.py
+++ solution.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import pathlib
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, array_to_img, load_img
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import img_to_array, array_to_img, load_img
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
-from tensorflow.keras.models import Sequential
+from keras.utils import to_categorical
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+from keras.models import Sequential
 
 dataset_path = os.path.dirname(__file__) + "/Dataset"
 train_path = dataset_path + "/Train"
@@ -36,7 +37,7 @@ def load_images(path):
     for category in range(CATEGORIES_COUNT):
         categories_path = os.path.join(path, str(category))
         for img in os.listdir(categories_path):
-            img = load_img(os.path.join(categories_path, img), target_size=(30, 30))
+            img = load_img(os.path.join(categories_path, img), target_size=(IMG_WIDTH, IMG_HEIGHT))
             image = img_to_array(img)
             images_list.append(image)
             label_list.append(category)
@@ -45,3 +46,47 @@ def load_images(path):
 images_loaded, labels_loaded = load_images(train_path)
 labels_loaded = to_categorical(labels_loaded)
 x_train, x_test, y_train, y_test = train_test_split(np.array(images_loaded), labels_loaded, test_size=0.4)
+
+
+
+
+convModel = Sequential()
+
+
+convModel.add(Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=(IMG_HEIGHT,IMG_WIDTH,3)))
+convModel.add(MaxPool2D(pool_size=(2, 2)))
+convModel.add(Dropout(rate=0.25))
+
+
+convModel.add(Conv2D(filters=64, kernel_size=3, activation='relu'))
+convModel.add(MaxPool2D(pool_size=(2, 2)))
+convModel.add(Dropout(rate=0.25))
+
+convModel.add(Conv2D(filters=64, kernel_size=3, activation='relu'))
+
+convModel.summary()
+
+convModel.add(Flatten())
+convModel.add(Dense(units=64, activation='relu'))
+convModel.add(Dense(CATEGORIES_COUNT, activation='softmax'))
+
+convModel.summary()
+
+print("Kreiran model")
+convModel.compile(
+    loss='categorical_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy']
+)
+
+print("Kompajliran model")
+EPOCHS = 30
+fitted_model = convModel.fit(x_train, 
+                    y_train,
+                    validation_data = (x_test, y_test), 
+                    epochs=EPOCHS, 
+                    steps_per_epoch=60
+                   )
+
+print("Zavrsio model")
+
